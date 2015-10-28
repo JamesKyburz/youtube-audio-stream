e140bf5661468a6a2cff12a67155cab4c2aed7bd
Jeff Sisson <jeffsisson@gmail.com>
2015-10-28 18:53:08 -0400

    forward the stream error on if error listeners are present, otherwise throw an error

--- index.js
+++ index.js
@@ -20,9 +20,14 @@ function streamify(uri, opt) {
     :
     through();
 
-  new ffmpeg({source: video})
-    .on('error', function() {
+  var f = new ffmpeg({source: video})
+    .on('error', function(err) {
       video.end();
+      if (stream.listeners('error').length > 2) {
+        stream.emit('error', err);
+      } else {
+        throw new Error(err);
+      }
     })
     .toFormat(opt.audioFormat)
     .writeToStream(stream)
