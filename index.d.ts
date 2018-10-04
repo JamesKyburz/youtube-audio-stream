87f0474c0d4212e15c4daf2dd26a3db6507e8ad3
Fazendaaa <lucas.carotta@outlook.com>
2018-10-03 22:27:16 -0300

    Adding TS typings.

---
+++ index.d.ts
@@ -0,0 +1,26 @@
+declare module 'youtube-audio-stream' {
+    import ytdl from 'ytdl-core';
+    import Ffmpeg from 'fluent-ffmpeg'
+    import { PathLike, WriteStream } from 'fs';
+
+    export interface Format {
+        readonly container: string;
+        readonly audioEncoding;
+    }
+
+    export interface Options extends ytdl.downloadOptions {
+        readonly file?: PathLike;
+        readonly videoFormat?: string;
+        readonly audioFormat?: string;
+        readonly applyOptions?: (input: Ffmpeg.FfmpegCommand) => void;
+    }
+
+    /**
+     * Fetches the audio from the YouTube video.
+     * 
+     * @param uri YouTube video link.
+     * @param opt Options to send to process the audio in the video.
+     * @returns A audio stream.
+     */
+    export default function streamify(uri: string, opt?: Options): WriteStream;
+}
