4f53b83295d14fc28e9d329e7faa6b4d4ca12937
James Kyburz <james.kyburz@gmail.com>
2018-12-29 17:17:06 +0100

    remove xtend

--- index.js
+++ index.js
@@ -1,38 +1,29 @@
 var ytdl = require('ytdl-core')
 var FFmpeg = require('fluent-ffmpeg')
 var through = require('through2')
-var xtend = require('xtend')
 var fs = require('fs')
 
 module.exports = streamify
 
 function streamify (uri, opt) {
-  opt = xtend({
+  opt = {
+    ...opt,
     videoFormat: 'mp4',
     quality: 'lowest',
     audioFormat: 'mp3',
-    filter: filterVideo,
+    filter (format) {
+      return format.container === opt.videoFormat && format.audioEncoding
+    },
     applyOptions: function () {}
-  }, opt)
+  }
 
   var video = ytdl(uri, opt)
 
-  function filterVideo (format) {
-    return (
-      format.container === opt.videoFormat &&
-      format.audioEncoding
-    )
-  }
-
-  var stream = opt.file
-    ? fs.createWriteStream(opt.file)
-    : through()
+  var stream = opt.file ? fs.createWriteStream(opt.file) : through()
 
   var ffmpeg = new FFmpeg(video)
   opt.applyOptions(ffmpeg)
-  var output = ffmpeg
-    .format(opt.audioFormat)
-    .pipe(stream)
+  var output = ffmpeg.format(opt.audioFormat).pipe(stream)
 
   video.on('info', stream.emit.bind(stream, 'info'))
   ffmpeg.on('error', stream.emit.bind(stream, 'error'))
